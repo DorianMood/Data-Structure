f98f000260137120f2dae70bb1ea651fec8f62cf
nekithockey75 <dorianmood@163.com>
2020-05-04 13:03:36 +0500

    Lab5 done

--- Lab5/main.cpp
+++ Lab5/main.cpp
@@ -1,4 +1,13 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <stack>
+
+bool correctNestedPlaced(std::string);
+
+#define helperOpen(c) c == '(' || c == '{' || c == '['
+#define helperClose(c) c == ')' || c == '}' || c == ']'
+#define closeOpen(c) c == ')' ? '(' : (c == '}' ? '{' : '[')
 
 int main()
 {
@@ -8,5 +17,33 @@ int main()
     Output ¡°TRUE¡± if these brackets are properly nested and placed.
     Otherwise output ¡°FALSE¡±
     */
+    std::ifstream fs("input.txt", std::fstream::in);
+    std::string line;
 
+    while (std::getline(fs, line))
+    {
+        std::cout << (correctNestedPlaced(line) ? "TRUE" : "FALSE") << std::endl;
+    }
+}
+
+bool correctNestedPlaced(std::string s)
+{
+    std::stack<char> st;
+
+    for (std::string::iterator c = s.begin(); c != s.end(); c++)
+    {
+        if (helperOpen(*c))
+        {
+            st.push(*c);
+        }
+        else if (helperClose(*c))
+        {
+            if ((st.size() > 0) && (closeOpen(*c) == st.top()))
+                st.pop();
+            else
+                return false;
+        }
+    }
+
+    return true;
 }
