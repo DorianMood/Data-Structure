789baeebfa3c49caac9f2bc0f4d7e43419d22f75
nekithockey75 <dorianmood@163.com>
2020-05-05 23:48:23 +0500

    lab7 done

--- Lab7/SortedList.h
+++ Lab7/SortedList.h
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <map>
+#include <algorithm>
 
 #include "Dictionary.h"
 
@@ -21,9 +22,7 @@ public:
     void Output(std::ostream& out) const;
 
 private:
-    std::map<const K, E> elements;
-
-    std::pair<const K, E>* x;
+    std::pair<K, E>* x;
     int size;
     int capacity;
 
@@ -35,7 +34,7 @@ template <class K, class E>
 SortedList<K, E>::SortedList()
 {
     capacity = INITIAL_CAPACITY;
-    x = new std::pair<const K, E>[capacity];
+    x = new std::pair<K, E>[capacity];
     size = 0;
 }
 
@@ -48,7 +47,6 @@ SortedList<K, E>::~SortedList()
 template <class K, class E>
 std::pair<const K, E>* SortedList<K, E>::Find(const K& e) const
 {
-    // TODO : rewrite for std::map
     // We can use binary search
     int left = 0, right = size - 1;
     while (left <= right)
@@ -58,8 +56,8 @@ std::pair<const K, E>* SortedList<K, E>::Find(const K& e) const
             left = middle + 1;
         else if (x[middle].second > e)
             right = middle - 1;
-        else
-            return &x[middle];
+        else // TODO : fix this, return actual pair not copy.
+            return &std::pair<const K, E>(x[middle].first, x[middle].second);
     }
 
     return nullptr;
@@ -69,16 +67,37 @@ template <class K, class E>
 void SortedList<K, E>::Erase(const K& key)
 {
     // Delete from correct position
-    elements.erase(key);
-    return;
+
+    int actualIndex = 0;
+    for (; actualIndex < size && x[actualIndex].first != key; actualIndex++);
+
+    // No element found
+    if (actualIndex == size)
+        return;
+
+    for (int i = actualIndex; i < size - 1; i++)
+        x[i] = x[i + 1];
+
+    size--;
 }
 
 template <class K, class E>
 void SortedList<K, E>::Insert(const pair<const K, E>& item)
 {
-    // Insert to the correct position
-    elements.insert(item);
-    return;
+    // Make 2 bigger
+    if (size == capacity)
+    {
+        capacity *= 2;
+        std::pair<K, E>* newX = new std::pair<K, E>[capacity];
+        for (int i = 0; i < size; i++)
+            newX[i] = x[i];
+        x = newX;
+    }
+    
+    x[size] = item;
+    size++;
+
+    std::sort(x, x + size);
 }
 
 template <class K, class E>
@@ -86,7 +105,7 @@ void SortedList<K, E>::Output(std::ostream& out) const
 {
     for (int i = 0; i < size; i++)
     {
-        std::cout << elements[i]->first << " " << elements[i]->second << "\t";
+        std::cout << x[i].first << " " << x[i].second << "\t";
     }
     std::cout << std::endl;
 }
--- Lab7/main.cpp
+++ Lab7/main.cpp
@@ -9,12 +9,33 @@ int main()
     Provide the same member functions as provided the class SortedChain .
     Write code for all functions and test the code using suitable test data.
     */
+
     SortedList<int, int>* list = new SortedList<int, int>();
 
-    list->Insert(std::make_pair(1, 1));
+    // Test Intertion.
+
     list->Insert(std::make_pair(2, 1));
     list->Insert(std::make_pair(3, 1));
+    list->Insert(std::make_pair(1, 1));
     list->Insert(std::make_pair(4, 1));
+
+    list->Output(std::cout);
+
+    // Test Erase.
+
+    list->Erase(3);
+    list->Erase(4);
+    list->Erase(1);
+    list->Erase(1);
+
+    list->Output(std::cout);
+    
+    //  Test Insert again.
+
+    list->Insert(std::make_pair(5, 12));
+    list->Insert(std::make_pair(0, -12));
+
+    list->Output(std::cout);
 
     return 0;
 }
