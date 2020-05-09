e58f77f5059038157b90a5c76f27ac6f7c22a422
nekithockey75 <dorianmood@163.com>
2020-05-09 21:06:30 +0500

    Max priority queue list implementation

---
+++ Lab9/MaxPriorityQueueList.h
@@ -0,0 +1,18 @@
+#ifndef MAX_PRIORITY_QUEUE_
+#define MAX_PRIORITY_QUEUE_
+
+#include <iostream>
+
+#include "MaxPriorityQueue.h"
+
+template <class T>
+class MaxPriorityQueueList : maxPriorityQueue
+{
+public:
+
+private:
+
+
+};
+
+#endif // !MAX_PRIORITY_QUEUE_
--- Lab9/main.cpp
+++ Lab9/main.cpp
@@ -37,7 +37,13 @@ int main()
 
 	tree->Output();
 
+	/**
+	1. Develop a C++ class for the ADT MaxPriorityQueue
+	using an unordered formula-based linear list (i.e, use the class LinearList of Program 3.1).
+	The insert time should be жи(1), and the delete max time should be O(n) where n is the number of elements in the queue.
 
+	2. Do Exercise 1 using an unordered chain (i.e, use the class Chain of Program 3.8).
+	*/
 
 	return 0;
 }
